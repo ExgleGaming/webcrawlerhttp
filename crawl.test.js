feb268b54944bf2cb9dff51593f80c4b04c9212f
ExgleGaming <EaglesFan91x@outlook.com>
2024-05-31 21:37:13 -0400

    error fixing

---
+++ crawl.test.js
@@ -0,0 +1,12 @@
+import { test, expect } from "@jest/globals";
+import { normalizeURL } from "./crawl.js";
+
+
+
+test('normalizeURL', () => {
+    const input = ''
+    const actual = normalizeURL(input)
+    const expected = ''
+    expect(actual).toEqual(expected)
+})
+
